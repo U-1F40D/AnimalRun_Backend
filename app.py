ab26508bedffd8684ab76fe2ea4a63c9b4c87f05
kaem <konmarciniak@gmail.com>
2022-07-14 11:04:07 +0200

    initial idea for data acquirement

---
+++ app.py
@@ -0,0 +1,25 @@
+# import main Flask class and request object
+from flask import Flask, request
+
+response = ''
+
+# create the Flask app
+app = Flask(__name__)
+
+
+@app.route('/classify-animal', methods=['GET', 'POST'])
+def classify_animal():
+
+    global response
+
+    if request.method == 'POST':
+        request_data = request.data
+    # encoded Base64
+
+    # decode data
+
+    return 'Decoded Base64 Object'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
